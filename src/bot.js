3726862cca2faae7cc18a8ff96abc940f4c59e03
Rhys Sharrem <rhys.sharrem@gmail.com>
2018-05-01 22:37:45 +0100

    Add now-cli deploy

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,28 @@
 const Twit = require('twit');
 const config = require('./config');
 const moment = require('moment');
+const http = require('http');
+
+
 const bot = new Twit(config);
 const helloWorld = 'Hello World! May hasn\'t finished yet';
 const myHandle = 'HasMayFinished';
 const june = moment('2018-06-01 00:00:00')
 const hasMayResigned = false;
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 tweetStatus = function(tweet) {
 	bot.post('statuses/update', tweet, (err, data, response) => {
@@ -24,7 +41,6 @@ followed = function(eventMessage) {
 	}
 	tweetStatus(reply)
 }
-
 
 tweetReceived = function(tweet) {
 	var recipient = tweet.in_reply_to_screen_name;
